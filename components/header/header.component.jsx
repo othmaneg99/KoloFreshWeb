c7be0a13d4afd79d04cd1f45aae237b60d233a75
Othmane <gougiliotman@gmail.com>
2022-01-06 18:50:56 +0100

    cooker page and footer section first commit

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -7,17 +7,18 @@ const Header = props => {
   const style = {
     btnSignIn: {
       width: '183px',
-      height: '78px',
+      height: '47px',
+      margin : '20px'
     },
   };
 
   return (
     <header>
       <Grid container spacing={24}>
-        <Grid item xs={6}>
-          <Image src={logoSvg} height={86} width={212} alt='Kolo Fresh' />
+        <Grid item xs={6} >
+          <Image src={logoSvg}  width="160px" alt='Kolo Fresh' />
         </Grid>
-        <Grid item xs={6} style={{ textAlign: 'right' }}>
+        <Grid item xs={6} style={{ textAlign: 'right'}}>
           <ButtonRed style={style.btnSignIn}>Sign In</ButtonRed>
         </Grid>
       </Grid>
--- pages/cooker.jsx
+++ pages/cooker.jsx
@@ -1,12 +1,102 @@
 import Footer from '@components/footer/footer.component';
 import Header from '@components/header/header.component';
+import Image from 'next/image';
+import step1 from '@assets/images/step1.svg';
+import step2 from '@assets/images/step2.svg';
+import step3 from '@assets/images/step3.svg';
+import done from '@assets/images/done.svg';
+import tomato from '@assets/images/tomato.svg';
+import plant from '@assets/images/plant.svg';
+import ButtonRed from '@components/button/buttonRed.component';
+import styles from '/styles/cooker.module.scss';
+import Box from '@mui/material/Box';
+import ReactDOM from 'react-dom';
+import { Hidden } from '@mui/material';
+import React, { useState } from 'react';
+import {capitalizeFirstLetter} from '@utils/global'
 
 export default function Cooker() {
+  const [step, setStep] = useState(1);
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  
   return (
-    <div className='container'>
-      <Header />
-      <main className='mainContent'></main>
-      <Footer />
-    </div>
+    <div className='container' >
+      <Header/>
+      <Image src={tomato} className={styles.tomatoImage} />
+      <Image src={plant} className={styles.plantImage} />
+      {step == 1 && 
+        <main id='step1' className='mainContent'>
+          <Image src={step1} />
+        <form action='' autoComplete='off'>
+          <div className={styles.formRow}>
+            <div className={styles.inputWrap}>
+              <label className={styles.label} htmlFor='nom'>
+                Nom*
+              </label>
+              <input className={styles.input} type='text' name='nom' id='' onChange={(event)=>{setNom(event.target.value.toUpperCase())}}required />
+            </div>
+            <div className={styles.inputWrap}>
+              <label className={styles.label} htmlFor='prenom'>
+                Prénom*
+              </label>
+              <input className={styles.input} type='text' name='prenom' id='' onChange={(event)=>{setPrenom(capitalizeFirstLetter(event.target.value))}}required />
+            </div>
+          </div>
+          <Box textAlign='center'>
+            <ButtonRed onClick={()=>{setStep(2)}} className={styles.btn}>
+              Suivant
+            </ButtonRed>
+          </Box>
+        </form>
+      </main>
+      
+      }
+
+
+      {step == 2 && 
+      <main  id='step2' className='mainContent'>
+        <Image src={step2} />
+        <form action='' autoComplete='off'>
+          <div className={styles.formRow}>
+            <div className={styles.inputWrap}>
+              <label className={styles.label} htmlFor='nom'>
+                Email*
+              </label>
+              <input className={styles.input} type='email' name='nom' id='' required />
+            </div>
+            <div className={styles.inputWrap}>
+              <label className={styles.label} htmlFor='prenom'>
+                Numéro de telèphone*
+              </label>
+              <input className={styles.input} type='text' name='prenom' id='' required />
+            </div>
+          </div>
+          <Box textAlign='center'>
+            <ButtonRed onClick={()=>{setStep(3)}} className={styles.btn}>
+              Suivant
+            </ButtonRed>
+          </Box>
+        </form>
+      </main>
+      }
+      {step == 3 && 
+      <main  className='mainContent'>
+        <Image src={step3} />
+        <Image src={done} />
+        <h1>Félicitations {nom} {prenom} !</h1>
+        <p className={styles.paragraph}>
+          Votre inscription a bien été prise en compte. Vous serez contacté par 
+           <text style={{color: '#C32530'}}> un agent de notre centre d'appel</text> dans les 
+         <b style={{color: '#C32530'}}> 24 heures</b> a venir afin de confirmer vos informations et vous communiquer votre <span style={{color: '#C32530'}}>mot de passe</span>.
+        </p> 
+        
+        
+      </main>
+      }
+
+    <Footer/>
+    </div>    
   );
+
 }
