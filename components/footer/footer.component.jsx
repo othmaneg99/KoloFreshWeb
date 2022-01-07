a2d06aecd4f0f73f0a3c6a5c9e60598034057b20
Othmane <gougiliotman@gmail.com>
2022-01-07 14:35:38 +0100

    cooker page and footer section #2

--- components/footer/footer.component.jsx
+++ components/footer/footer.component.jsx
@@ -7,75 +7,70 @@ import applestore from '@assets/images/applestore.svg';
 import curve from '@assets/images/curve.svg';
 
 const Footer = () => {
+  const footerContainer = {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  };
 
-const footerContainer = {
-      
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
+  const footer = {
+    position: 'relative',
+  };
+  const curve1 = {};
+  const footerSection = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  };
 
-}
+  return (
+    <footer className='footer'>
+      <Image src={curve} width={''} className='curve' sizes='(max-width: 100%)' />
+      <div className='footerContainer'>
+        <div style={footerSection}>
+          <p className='title'>KOLO FRESH</p>
+          <p style={{ maxWidth: '25ch' }}>It is a new Moroccan concept that allows you to order meals, especially homemade dishes.</p>
+        </div>
 
-const footer = {
-    position: 'relative'
+        <div style={footerSection}>
+          <p className='title'>Quick links</p>
+          <a href=''>Home</a>
+          <a href=''>Shops</a>
+          <a href=''>Nous contacter</a>
+        </div>
 
-}
-const curve1 = {
+        <div style={footerSection}>
+          <p className='title'>Nous suivre</p>
+          <a href=''>Facebook</a>
+          <a href=''>Instagram</a>
+          <a href=''>Twitter</a>
+          <a href=''>Linkedin</a>
+        </div>
 
-
-}
-const footerSection = {
-
-  display : 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between'
-
-}
-
-
-  return <footer className='footer'>
-    <Image src={curve} width={''} className='curve' sizes="(max-width: 100%)"/>
-    <div className='footerContainer'>
-   
-      <div style={footerSection}>
-      <p className='title'>KOLO FRESH</p>
-      <p style={{maxWidth: '25ch'}}>It is a new Moroccan concept that allows you to order meals, especially homemade dishes.</p>
-
-    </div>
-
-    <div style={footerSection}>
-      <p className='title'>Quick links</p>
-      <a href="">Home</a>
-      <a href="">Shops</a>
-      <a href="">Nous contacter</a>
-    </div>
-
-    <div style={footerSection}>
-      <p className='title'>Nous suivre</p>
-      <a href="">Facebook</a>
-      <a href="">Instagram</a>
-      <a href="">Twitter</a>
-      <a href="">Linkedin</a>
-    </div>
-
-    <div style={footerSection}>
-      
-      <a href=""><Image src={playstore} alt='playstore' /></a>
-      <a href=""><Image src={applestore} alt='applestore' /></a>
-      <a href="">CONDITIONS <br />D'UTILISATION</a>
-      <a href="">POLITIQUE DE <br />CONFIDENTIALITE</a>
-      
-    </div>
- </div>
- <div className='copywrite'>
-     <div style={{ borderTop: "2px solid #8F9698 ",width: '80%',margin: '0 auto',paddingBottom: '20px' }}></div>
-    <span style={{ display: 'table',margin: '0 auto',paddingBottom: '20px'}}>Copyright 2021 ©CodeCreators  All rights reserved.</span>
- </div>
-
-
-
-  </footer>;
+        <div style={footerSection}>
+          <a href=''>
+            <Image src={playstore} alt='playstore' />
+          </a>
+          <a href=''>
+            <Image src={applestore} alt='applestore' />
+          </a>
+          <a href=''>
+            CONDITIONS <br />
+            D'UTILISATION
+          </a>
+          <a href=''>
+            POLITIQUE DE <br />
+            CONFIDENTIALITE
+          </a>
+        </div>
+      </div>
+      <div className='copywrite'>
+        <div style={{ borderTop: '2px solid #8F9698 ', width: '80%', margin: '0 auto', paddingBottom: '20px' }}></div>
+        <span style={{ display: 'table', margin: '0 auto', paddingBottom: '20px' }}>Copyright 2021 ©CodeCreators All rights reserved.</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- pages/cooker.jsx
+++ pages/cooker.jsx
@@ -13,90 +13,116 @@ import Box from '@mui/material/Box';
 import ReactDOM from 'react-dom';
 import { Hidden } from '@mui/material';
 import React, { useState } from 'react';
-import {capitalizeFirstLetter} from '@utils/global'
+import { capitalizeFirstLetter } from '@utils/global';
 
 export default function Cooker() {
   const [step, setStep] = useState(1);
-  const [nom, setNom] = useState("");
-  const [prenom, setPrenom] = useState("");
-  
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+
   return (
-    <div className='container' >
-      <Header/>
+    <div className='container'>
+      <Header />
       <Image src={tomato} className={styles.tomatoImage} />
       <Image src={plant} className={styles.plantImage} />
-      {step == 1 && 
+      {step == 1 && (
         <main id='step1' className='mainContent'>
           <Image src={step1} />
-        <form action='' autoComplete='off'>
-          <div className={styles.formRow}>
-            <div className={styles.inputWrap}>
-              <label className={styles.label} htmlFor='nom'>
-                Nom*
-              </label>
-              <input className={styles.input} type='text' name='nom' id='' onChange={(event)=>{setNom(event.target.value.toUpperCase())}}required />
+          <form action='' autoComplete='off'>
+            <div className={styles.formRow}>
+              <div className={styles.inputWrap}>
+                <label className={styles.label} htmlFor='nom'>
+                  Nom*
+                </label>
+                <input
+                  className={styles.input}
+                  type='text'
+                  name='nom'
+                  id=''
+                  onChange={event => {
+                    setNom(event.target.value.toUpperCase());
+                  }}
+                  required
+                />
+              </div>
+              <div className={styles.inputWrap}>
+                <label className={styles.label} htmlFor='prenom'>
+                  Prénom*
+                </label>
+                <input
+                  className={styles.input}
+                  type='text'
+                  name='prenom'
+                  id=''
+                  onChange={event => {
+                    setPrenom(capitalizeFirstLetter(event.target.value));
+                  }}
+                  required
+                />
+              </div>
             </div>
-            <div className={styles.inputWrap}>
-              <label className={styles.label} htmlFor='prenom'>
-                Prénom*
-              </label>
-              <input className={styles.input} type='text' name='prenom' id='' onChange={(event)=>{setPrenom(capitalizeFirstLetter(event.target.value))}}required />
-            </div>
-          </div>
-          <Box textAlign='center'>
-            <ButtonRed onClick={()=>{setStep(2)}} className={styles.btn}>
-              Suivant
-            </ButtonRed>
-          </Box>
-        </form>
-      </main>
-      
-      }
+            <Box textAlign='center'>
+              <ButtonRed
+                onClick={() => {
+                  setStep(2);
+                }}
+                className={styles.btn}
+              >
+                Suivant
+              </ButtonRed>
+            </Box>
+          </form>
+        </main>
+      )}
 
-
-      {step == 2 && 
-      <main  id='step2' className='mainContent'>
-        <Image src={step2} />
-        <form action='' autoComplete='off'>
-          <div className={styles.formRow}>
-            <div className={styles.inputWrap}>
-              <label className={styles.label} htmlFor='nom'>
-                Email*
-              </label>
-              <input className={styles.input} type='email' name='nom' id='' required />
+      {step == 2 && (
+        <main id='step2' className='mainContent'>
+          <Image src={step2} />
+          <form action='' autoComplete='off'>
+            <div className={styles.formRow}>
+              <div className={styles.inputWrap}>
+                <label className={styles.label} htmlFor='nom'>
+                  Email*
+                </label>
+                <input className={styles.input} type='email' name='nom' id='' required />
+              </div>
+              <div className={styles.inputWrap}>
+                <label className={styles.label} htmlFor='prenom'>
+                  Numéro de telèphone*
+                </label>
+                <input className={styles.input} type='text' name='prenom' id='' required />
+              </div>
             </div>
-            <div className={styles.inputWrap}>
-              <label className={styles.label} htmlFor='prenom'>
-                Numéro de telèphone*
-              </label>
-              <input className={styles.input} type='text' name='prenom' id='' required />
-            </div>
-          </div>
-          <Box textAlign='center'>
-            <ButtonRed onClick={()=>{setStep(3)}} className={styles.btn}>
-              Suivant
-            </ButtonRed>
-          </Box>
-        </form>
-      </main>
-      }
-      {step == 3 && 
-      <main  className='mainContent'>
-        <Image src={step3} />
-        <Image src={done} />
-        <h1>Félicitations {nom} {prenom} !</h1>
-        <p className={styles.paragraph}>
-          Votre inscription a bien été prise en compte. Vous serez contacté par 
-           <text style={{color: '#C32530'}}> un agent de notre centre d'appel</text> dans les 
-         <b style={{color: '#C32530'}}> 24 heures</b> a venir afin de confirmer vos informations et vous communiquer votre <span style={{color: '#C32530'}}>mot de passe</span>.
-        </p> 
-        
-        
-      </main>
-      }
+            <Box textAlign='center'>
+              <ButtonRed
+                onClick={() => {
+                  setStep(3);
+                }}
+                className={styles.btn}
+              >
+                Suivant
+              </ButtonRed>
+            </Box>
+          </form>
+        </main>
+      )}
+      {step == 3 && (
+        <main className='mainContent'>
+          <Image src={step3} />
+          <Image src={done} />
+          <h1>
+            Félicitations {nom} {prenom} !
+          </h1>
+          <p className={styles.paragraph}>
+            Votre inscription a bien été prise en compte. Vous serez contacté par
+            <text style={{ color: '#C32530' }}> un agent de notre centre d'appel</text> dans les
+            <b style={{ color: '#C32530' }}> 24 heures</b> a venir afin de confirmer vos informations et vous communiquer votre{' '}
+            <span style={{ color: '#C32530' }}>mot de passe</span>.
+          </p>
+        </main>
+      )}
 
-    <Footer/>
-    </div>    
+      <Footer />
+    </div>
   );
-
 }
