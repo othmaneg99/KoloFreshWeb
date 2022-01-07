1375678c4f58d0cbdad4d2471bfbbda4fe10c641
Ilyas <ilmakroum@gmail.com>
2022-01-07 14:43:47 +0100

    Create home page #1

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,8 +1,11 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '/styles/home.module.scss';
 import { Grid } from '@mui/material';
 import mintSvg from '@assets/images/mint.svg';
+import ellipse from '@assets/images/ellipse.svg';
+import tajine from '@assets/images/tajine.svg';
 import Header from '@components/header/header.component';
 import Footer from '@components/footer/footer.component';
 
@@ -15,6 +18,7 @@ export default function Home() {
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'></meta>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
       <Header />
       <main className='mainContent'>
         <Grid container spacing={24}>
@@ -27,9 +31,13 @@ export default function Home() {
               Browse from our list of specials to place your order and have food delivered to you in no time. Affordable, tasty and fast!
             </p>
           </Grid>
-          <Grid item xs={6}></Grid>
+          <Grid item xs={6}>
+            <Image src={tajine} width={400} alt='Tajine' className={styles.tajineImage} />
+            <Image src={ellipse} width={400} alt='Ellipse' className={styles.ellipseImage} />
+          </Grid>
         </Grid>
       </main>
+
       <Footer />
     </div>
   );
