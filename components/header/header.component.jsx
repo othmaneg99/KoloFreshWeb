3ac569779a089b6323b2926633466574ebc5c1e8
Ilyas <ilmakroum@gmail.com>
2022-01-07 14:41:55 +0100

    Create header component #1

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -5,20 +5,23 @@ import Image from 'next/image';
 
 const Header = props => {
   const style = {
+    header: {
+      padding: '2.5rem 2.5rem',
+    },
     btnSignIn: {
       width: '183px',
       height: '47px',
-      margin : '20px'
+      margin: '20px',
     },
   };
 
   return (
-    <header>
+    <header style={style.header}>
       <Grid container spacing={24}>
-        <Grid item xs={6} >
-          <Image src={logoSvg}  width="160px" alt='Kolo Fresh' />
+        <Grid item xs={6}>
+          <Image src={logoSvg} width='160px' alt='Kolo Fresh' />
         </Grid>
-        <Grid item xs={6} style={{ textAlign: 'right'}}>
+        <Grid item xs={6} style={{ textAlign: 'right' }}>
           <ButtonRed style={style.btnSignIn}>Sign In</ButtonRed>
         </Grid>
       </Grid>
