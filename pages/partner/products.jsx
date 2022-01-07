87fb2e5ac7f084355c9ec71548f532dc9ccc1eb0
Ilyas <ilmakroum@gmail.com>
2022-01-07 14:52:36 +0100

    Init partner products page

---
+++ pages/partner/products.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import styles from '/styles/partnerProducts.module.scss';
+
+export default function PartnerProducts() {
+  return (
+    <div className={styles.container}>
+      <h1>Partner products</h1>
+    </div>
+  );
+}
