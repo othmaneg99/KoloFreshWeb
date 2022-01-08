87207374c153eecc967ec3b0f9f5fcb8ffc72dd9
Ilyas <ilmakroum@gmail.com>
2022-01-08 09:12:17 +0100

    Create home page #2

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '/styles/home.module.scss';
-import { Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import mintSvg from '@assets/images/mint.svg';
 import ellipse from '@assets/images/ellipse.svg';
 import tajine from '@assets/images/tajine.svg';
@@ -11,7 +11,7 @@ import Footer from '@components/footer/footer.component';
 
 export default function Home() {
   return (
-    <div className='container'>
+    <div>
       <Head>
         <title>Kolo Fresh</title>
         <meta name='description' content='Kolo Fresh' />
@@ -21,21 +21,23 @@ export default function Home() {
 
       <Header />
       <main className='mainContent'>
-        <Grid container spacing={24}>
-          <Grid item xs={6}>
-            <Image src={mintSvg} height={50} width={156} alt='Mint' className={styles.mintImage} />
-            <p className='mainText'>
-              commander des plats <span className='greenColor'>100%</span> fait maison
-            </p>
-            <p className='secondaryText'>
-              Browse from our list of specials to place your order and have food delivered to you in no time. Affordable, tasty and fast!
-            </p>
+        <Container>
+          <Grid container spacing={24}>
+            <Grid item xs={6}>
+              <Image src={mintSvg} height={50} width={156} alt='Mint' className={styles.mintImage} />
+              <p className='mainText'>
+                commander des plats <span className='greenColor'>100%</span> fait maison
+              </p>
+              <p className='secondaryText'>
+                Browse from our list of specials to place your order and have food delivered to you in no time. Affordable, tasty and fast!
+              </p>
+            </Grid>
+            <Grid item xs={6}>
+              <Image src={tajine} width={400} alt='Tajine' className={styles.tajineImage} />
+              {/* <Image src={ellipse} width={400} alt='Ellipse' className={styles.ellipseImage} /> */}
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Image src={tajine} width={400} alt='Tajine' className={styles.tajineImage} />
-            <Image src={ellipse} width={400} alt='Ellipse' className={styles.ellipseImage} />
-          </Grid>
-        </Grid>
+        </Container>
       </main>
 
       <Footer />
