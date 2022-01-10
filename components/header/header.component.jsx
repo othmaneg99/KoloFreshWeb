4c65dc34f4f16d486d749932194c4202a853efc3
Othmane <gougiliotman@gmail.com>
2022-01-11 00:27:46 +0100

    signin compomenet #1

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -1,9 +1,14 @@
+import { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 import logoSvg from '@assets/images/logo.svg';
 import ButtonRed from '@components/button/buttonRed.component';
 import Image from 'next/image';
+import SignIn from '@components/signIn/signIn.component';
 
 const Header = props => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const style = {
     header: {
       padding: '2.5rem 2.5rem',
@@ -26,7 +31,10 @@ const Header = props => {
           <Image src={logoSvg} width='160px' alt='Kolo Fresh' />
         </Grid>
         <Grid item xs={6} style={{ textAlign: 'right' }}>
-          <ButtonRed style={style.btnSignIn}>Sign In</ButtonRed>
+          <ButtonRed style={style.btnSignIn} onClick={handleOpen}>
+            Sign In
+          </ButtonRed>
+          <SignIn open={open} close={handleClose} />
         </Grid>
       </Grid>
     </header>
