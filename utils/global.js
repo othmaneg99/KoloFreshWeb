aa2a513cf48d103b2ee65766505a6cac2d59ac88
Othmane <gougiliotman@gmail.com>
2022-01-21 00:21:15 +0100

    sign in

--- utils/global.js
+++ utils/global.js
@@ -1,3 +1,12 @@
-export const capitalizeFirstLetter = (string) => {
+import { useRef, useEffect } from 'react';
+export const capitalizeFirstLetter = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
+
+export const useIsMount = () => {
+  const isMountRef = useRef(true);
+  useEffect(() => {
+    isMountRef.current = false;
+  }, []);
+  return isMountRef.current;
+};
