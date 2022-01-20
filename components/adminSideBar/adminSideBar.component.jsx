d2a8e5e494257746a5fed27c6d07f34f9a1835eb
Othmane <gougiliotman@gmail.com>
2022-01-20 15:55:50 +0100

    conflicts solving

--- components/adminSideBar/adminSideBar.component.jsx
+++ components/adminSideBar/adminSideBar.component.jsx
@@ -1,8 +1,13 @@
 import st from '/styles/partnerProducts.module.scss';
-
+import Image from 'next/image';
+import logo from '@assets/images/logo.svg';
+import logout from '@assets/images/logout.png';
 export default function AdminSideBar() {
   return (
     <div className={st.sideBar}>
+      <a href='/' className={st.pagesButtons}>
+        <Image src={logo} alt='logo' width='170' />
+      </a>
       <div style={{ paddingTop: '50%', height: '50%' }}>
         <div className={st.pagesButtonsDiv}>
           <a href='#' className={st.pageButtonActive}>
@@ -21,7 +26,8 @@ export default function AdminSideBar() {
             PARAMETRES
           </a>
           <a href='#' className={st.signOutBtn}>
-            Se déconnecter
+            <Image width={'18px'} height={'18px'} src={logout} />
+            <p style={{ display: 'inline', marginTop: '50px' }}> Se déconnecter </p>
           </a>
         </div>
       </div>
--- components/sideBar/sideBar.component.jsx
+++ components/sideBar/sideBar.component.jsx
@@ -3,49 +3,39 @@ import logo from '@assets/images/LOGO.png';
 import logout from '@assets/images/logout.png';
 import Image from 'next/image';
 
-export default function SideBar(){
-
-
-    return (
+export default function SideBar() {
+  return (
     <div className={st.sideBar}>
+      <a href='/' className={st.pagesButtons}>
+        <Image src={logo} />
+      </a>
 
-        <a href='/' className={st.pagesButtons}>
-            <Image src={logo}/>
-        </a>
-
-        <div style={{paddingTop:"10%"}}>
-
-            <div className={st.pagesButtonsDiv}>
-
-                <a href='#' className={st.pageButtonActive}>
-                    Produits
-                </a>
-                <a href='#' className={st.pagesButtons}>
-                    Commandes
-                </a>
-                <a href='#' className={st.pagesButtons}>
-                    Statistiques
-                </a>
-                <a href='#' className={st.pagesButtons}>
-                    Paramètres
-                </a>
-                <a href='#' className={st.pagesButtons}>
-                    Shop
-                </a>
-                <a href='#' className={st.pagesButtons}>
-                    Aide
-                </a>
-                <a href='#' className={st.signOutBtn}>
-                    
-         
-                    <Image  width={"18px"} height={"18px"} src={logout}/>
-                   <p style={{display:"inline", marginTop:"50px"}}>  Se déconnecter </p>
-                
-                </a>
-                
-            </div>
+      <div style={{ paddingTop: '10%' }}>
+        <div className={st.pagesButtonsDiv}>
+          <a href='#' className={st.pageButtonActive}>
+            Produits
+          </a>
+          <a href='#' className={st.pagesButtons}>
+            Commandes
+          </a>
+          <a href='#' className={st.pagesButtons}>
+            Statistiques
+          </a>
+          <a href='#' className={st.pagesButtons}>
+            Paramètres
+          </a>
+          <a href='#' className={st.pagesButtons}>
+            Shop
+          </a>
+          <a href='#' className={st.pagesButtons}>
+            Aide
+          </a>
+          <a href='#' className={st.signOutBtn}>
+            <Image width={'18px'} height={'18px'} src={logout} />
+            <p style={{ display: 'inline', marginTop: '50px' }}> Se déconnecter </p>
+          </a>
         </div>
-
+      </div>
     </div>
-    )
+  );
 }
