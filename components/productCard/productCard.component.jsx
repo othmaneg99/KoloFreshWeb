362947263beac5c1dad5ffcfdb1ff5970805d1c7
khaoulabenasila <k.benasila@mundiapolis.ma>
2022-01-20 20:30:56 +0100

    Product Changes

--- components/productCard/productCard.component.jsx
+++ components/productCard/productCard.component.jsx
@@ -1,48 +1,42 @@
 import Button from '@mui/material/Button';
 import Image from 'next/image';
 import st from '/styles/partnerProducts.module.scss';
+import bastilla from '@assets/images/bastilla.svg';
 
-export default function ProductCard(props){
+export default function ProductCard(props) {
+  console.log(props);
 
-    return (
-    
+  return (
     <div className={st.prodCard}>
-
-        <div className={st.spread}>
-
-            <div className={st.prodImgDiv}>
-                <Image
-                src={props.prodArray.image}
-                height={118} width={159} 
-                alt=''
-                />
-            </div>
-
-            <div>
-                <div>
-                <p className={st.prodName} style={{ marginBottom: '5px' }}>
-                    {props.prodArray.titre}
-                </p>
-                <p className={st.prodDescr} style={{ marginBottom: '5px', marginTop: '5px', marginLeft: '10px' }}>
-                    {props.prodArray.description}
-                </p>
-                </div>
-
-                <div className={st.actionButtons}>
-                <Button className={st.btnDesac} variant='contained'>
-                    Désactiver
-                </Button>
-                <Button className={st.btnDelete} variant='contained'>
-                    Supprimer
-                </Button>
-                </div>
-            </div>
-
-            <div>
-                <p className={st.prodPrice}>{props.prodArray.prix} DH</p>
-            </div>
-
+      <div className={st.spread}>
+        <div className={st.prodImgDiv}>
+          <Image className={st.prodImg} src={props.obj.imageProd.src} height={138} width={179} alt='' />
         </div>
 
-  </div>)
+        <div>
+          <div>
+            <p className={st.prodName} style={{ marginBottom: '10px' }}>
+              {props.obj.name}
+            </p>
+            <p className={st.prodDescr} style={{ marginBottom: '10px', marginTop: '5px', marginLeft: '10px', width: '100%' }}>
+              {props.obj.description}
+            </p>
+          </div>
+
+          <div className={st.actionButtons}>
+            <Button className={st.btnDesac} variant='contained'>
+              Désactiver
+            </Button>
+            <Button className={st.btnDelete} variant='contained'>
+              Supprimer
+            </Button>
+          </div>
+        </div>
+
+        <div>
+          <p className={st.price}>{props.obj.price} DH</p>
+        </div>
+      </div>
+    </div>
+  );
 }
